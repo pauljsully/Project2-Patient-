382413c0e8513f438caaac044cf9c80b45b26299
Juell Anthony <ju3llanthony@gmail.com>
2023-11-30 21:13:13 -0500

    seed formation

---
+++ Develop/seeds/index.js
@@ -0,0 +1,15 @@
+const sequelize = require('../config/connection');
+const seedUser = require('./userData');
+const seedPatient = require('./patientData');
+
+const seedAll = async () => {
+  await sequelize.sync({ force: true });
+
+  await seedUser();
+
+  await seedPatient();
+
+  process.exit(0);
+};
+
+seedAll();
--- Develop/seeds/userData.js
+++ Develop/seeds/userData.js
@@ -1,36 +1,25 @@
-// const { Gallery } = require('../models');
+--------------------------------------------------------------
 
-// const gallerydata = [
+// const { User } = require('../models');
+
+// const userdata = [
 //   {
-//     name: 'Printemps',
-//     starting_date: 'April 20, 2021 07:00:00',
-//     ending_date: 'June 21, 2021 17:00:00',
+//     username: 'Juell Anthony',
+//     email: '#',
+//     password: '#',
 //   },
 //   {
-//     name: 'Sommer',
-//     starting_date: 'June 22, 2021 09:00:00',
-//     ending_date: 'September 22, 2021 22:00:00',
+//     username: 'Paul Sulivan',
+//     email: '#',
+//     password: '#',
 //   },
 //   {
-//     name: 'Herfst',
-//     starting_date: 'September 23, 2021 08:30:00',
-//     ending_date: 'December 21, 2021 20:30:00',
-//   },
-//   {
-//     name: 'Invierno',
-//     starting_date: 'December 22, 2020 11:00:00',
-//     ending_date: 'March 19, 2021 19:00:00',
+//     username: 'Sammy Hassouneh',
+//     email: '#',
+//     password: '#',
 //   },
 // ];
 
-// const seedGallery = () => Gallery.bulkCreate(gallerydata);
+// const seedUser = () => User.bulkCreate(userdata);
 
-// module.exports = seedGallery;
-
-
---------------------------------------------------------------
-
-
-
-
-// this will hold our user information
+// module.exports = seedUser;
