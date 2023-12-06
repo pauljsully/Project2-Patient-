2308f72a2313147a95f220eff997a5b8afa6339c
Juell Anthony <ju3llanthony@gmail.com>
2023-12-06 15:10:04 -0500

    models looking good

--- models/index.js
+++ models/index.js
@@ -3,10 +3,11 @@ const Patient = require('./Patient');
 
 User.hasMany(Patient, {
   foreignKey: 'user_id',
+  onDelete: 'CASCADE'
 });
 
 Patient.belongsTo(User, {
-  foreignKey: 'user_id',
+  foreignKey: 'user_id'
 });
 
 module.exports = { User, Patient };
