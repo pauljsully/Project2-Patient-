fdbc50d5c54a64cb79fbde2b3d95b8d4b0e03344
Paul Sullivan <paul8sullivan@gmail.com>
2023-12-06 15:33:39 -0500

    added new photo and profile js

--- public/js/login.js
+++ public/js/login.js
@@ -1,42 +1,45 @@
 const loginFormHandler = async (event) => {
   event.preventDefault();
 
+  // Collect values from the login form
   const email = document.querySelector('#email-login').value.trim();
   const password = document.querySelector('#password-login').value.trim();
 
   if (email && password) {
-    const response = await fetch('/api/login/login', {
+    // Send a POST request to the API endpoint
+    const response = await fetch('/api/users/login', {
       method: 'POST',
       body: JSON.stringify({ email, password }),
       headers: { 'Content-Type': 'application/json' },
     });
 
     if (response.ok) {
-      document.location.replace('/patients');
+      // If successful, redirect the browser to the profile page
+      document.location.replace('/profile');
     } else {
-      alert('Failed to log in.');
+      alert(response.statusText);
     }
   }
 };
 
-const signupFormHandler = async (event) => {
+const registerFormHandler = async (event) => {
   event.preventDefault();
 
-  const username = document.querySelector('#username-signup').value.trim();
-  const email = document.querySelector('#email-signup').value.trim();
-  const password = document.querySelector('#password-signup').value.trim();
+  const name = document.querySelector('#name-register').value.trim();
+  const email = document.querySelector('#email-register').value.trim();
+  const password = document.querySelector('#password-register').value.trim();
 
-  if (username && email && password) {
+  if (name && email && password) {
     const response = await fetch('/api/users', {
       method: 'POST',
-      body: JSON.stringify({ username, email, password }),
+      body: JSON.stringify({ name, email, password }),
       headers: { 'Content-Type': 'application/json' },
     });
 
     if (response.ok) {
-      document.location.replace('/');
+      document.location.replace('/profile');
     } else {
-      alert('Failed to sign up.');
+      alert(response.statusText);
     }
   }
 };
@@ -46,5 +49,5 @@ document
   .addEventListener('submit', loginFormHandler);
 
 document
-  .querySelector('.signup-form')
-  .addEventListener('submit', signupFormHandler);
+  .querySelector('.register-form')
+  .addEventListener('submit', registerFormHandler);
