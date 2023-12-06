da2f2b1f1fdbb888a61546680e9ab9927cf68487
Paul Sullivan <paul8sullivan@gmail.com>
2023-12-05 22:19:02 -0500

    added to handlerbars

--- controllers/api/loginRoutes.js
+++ controllers/api/loginRoutes.js
@@ -30,6 +30,10 @@ router.post('/login', async (req, res) => {
       },
     });
 
+
+    console.log(req.body.password)
+    console.log(dbUserData.dataValues.password)
+
     if (!dbUserData) {
       res
         .status(400)
@@ -37,9 +41,11 @@ router.post('/login', async (req, res) => {
       return;
     }
 
-    const validPassword = await dbUserData.checkPassword(req.body.password);
+    // const validPassword = await dbUserData.checkPassword(req.body.password);
 
-    if (!validPassword) {
+    // console.log(validPassword)
+
+    if (req.body.password != dbUserData.dataValues.password) {
       res
         .status(400)
         .json({ message: 'Incorrect email or password. Please try again!' });
